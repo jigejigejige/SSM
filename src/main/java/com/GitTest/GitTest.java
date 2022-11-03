3f7b2ffee376023e0cc221ad521ab64db45fc9e6
ikun <ikun@123.com>
2022-11-03 14:55:22 +0800

    first commit

---
+++ src/main/java/com/GitTest/GitTest.java
@@ -0,0 +1,4 @@
+package com.GitTest;
+
+public class GitTest {
+}
---
+++ src/main/java/com/GitTest/GitTest1.java
@@ -0,0 +1,7 @@
+package com.GitTest;
+
+public class GitTest1 {
+    public static void main(String[] args) {
+        System.out.println("123");
+    }
+}
